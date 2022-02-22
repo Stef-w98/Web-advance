6d77ea39ffe1475d01e50347cd1ef37bb749e961
stef-w98 <stef.wouters22@gmail.com>
2022-02-22 15:28:58 +0100

    Commit

---
+++ Js deel 1/intro.js
@@ -0,0 +1,25 @@
+//alert('Welcome');
+
+//document.getElementById("voorbeeld").innerText = prompt("Geef je naam");
+
+//prompt("Geef je naam");
+//document.getElementById("voorbeeld").innerHTML = "<h1>Lorem ipsum.</H1>";
+//let myNumber = 6;
+
+//const zin1 = "Dit is een zin"
+//const zin2 = "Dit is een zin"
+//const zin3 = "Dit is een zin"
+//alert(zin1 + " " + zin2 + " " + zin3);
+
+
+let myNumberString = prompt("Geef een nummer");
+let myNumber = parseInt(myNumberString);
+
+let koekje = "Ik wil een koekje";
+
+console.log(koekje.charAt(0));
+console.log(koekje.charAt(3));
+console.log(koekje.lastIndexOf("k"))
+console.log(koekje.lastIndexOf("e"))
+
+console.log("Mijn eerste log");
