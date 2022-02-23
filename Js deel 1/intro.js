7ce2511d8cea45bcd7060e985b0011dbd70c7c92
stef-w98 <stef.wouters22@gmail.com>
2022-02-23 14:18:46 +0100

    Commit

--- Js deel 1/intro.js
+++ Js deel 1/intro.js
@@ -11,15 +11,27 @@
 //const zin3 = "Dit is een zin"
 //alert(zin1 + " " + zin2 + " " + zin3);
 
+let num1 = prompt("Geef een getal:");
+let num2 = prompt("Geef een 2de getal:");
 
-let myNumberString = prompt("Geef een nummer");
-let myNumber = parseInt(myNumberString);
+let resultaat1 = +num1 * +num2;
+let resultaat2 = +num1 / +num2;
+let resultaat3 = +num1 + +num2;
+let resultaat4 = +num1 - +num2;
+alert(`De vermenigvuldiging is ${resultaat1}`);
+alert(`De deling is ${resultaat2}`);
+alert(`De som is ${resultaat3}`);
+alert(`De aftrekking is ${resultaat4}`);
 
-let koekje = "Ik wil een koekje";
+//let myNumberString = prompt("Geef een woord");
+//alert(`de 1ste spatie begint bij karakter ${myNumberString.indexOf(" ")}`);
 
-console.log(koekje.charAt(0));
-console.log(koekje.charAt(3));
-console.log(koekje.lastIndexOf("k"))
-console.log(koekje.lastIndexOf("e"))
+//var pxLcatering = "lasagne van PXLcatering";
+//var n = koekje.length;
+//console.log(koekje.charAt(0));
+//console.log(koekje.charAt(3));
+//console.log(koekje.lastIndexOf("k"));
+//console.log(koekje.lastIndexOf("e"));
+//console.log(pxLcatering.toUpperCase());
 
-console.log("Mijn eerste log");
+//console.log("Mijn eerste log");
